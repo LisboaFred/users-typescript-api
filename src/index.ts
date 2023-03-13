19d7219d7022bb8ce864f07ad3297de2d1659fd9
LisboaFred <contato.fredericolisboa@gmail.com>
2023-03-13 11:16:54 -0300

    feat: add CreateUser to mongodb

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,32 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-users/mongo-create-users";
+import { CreateUserController } from "./controllers/create-user/create-user";
+import { createReadStream } from "fs";
 
 const main = async () => {
   config();
   const app = express();
+  app.use(express.json());
 
   await MongoClient.connect();
 
   app.get("/users", async (req, res) => {
     const mongoGetUsersRepository = new MongoGetUsersRepository();
-
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
-
     const { body, statusCode } = await getUsersController.handle();
+    res.send(body).status(statusCode);
+  });
 
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
     res.send(body).status(statusCode);
   });
 
